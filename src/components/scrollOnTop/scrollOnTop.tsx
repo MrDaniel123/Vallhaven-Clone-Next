6720b844c89218b32b4ddfe0fa786bd7e5d38ea5
Daniel <pandaniel13286@gmail.com>
2024-09-03 16:09:30 +0200

    Add Smooth scroll to the top of page

--- src/components/scrollOnTop/scrollOnTop.tsx
+++ src/components/scrollOnTop/scrollOnTop.tsx
@@ -4,15 +4,40 @@ import Image from 'next/image';
 import arrowIcon from '@/assets/ScrollArrow.png';
 
 import classes from './scrollOnTop.module.scss';
+import { useEffect, useState } from 'react';
 
 export default function ScrollOnTop() {
+	const [isVisible, setIsVisible] = useState(false);
+
 	function scrollOfTheTop() {
-		window.scrollTo(0, 0);
+		window.scrollTo({
+			top: 0,
+			behavior: 'smooth',
+		});
 	}
 
+	useEffect(() => {
+		// Button is displayed after scrolling for 500 pixels
+		const toggleVisibility = () => {
+			if (window.pageYOffset > 500) {
+				setIsVisible(true);
+			} else {
+				setIsVisible(false);
+			}
+		};
+
+		window.addEventListener('scroll', toggleVisibility);
+
+		return () => window.removeEventListener('scroll', toggleVisibility);
+	}, []);
+
 	return (
-		<button onClick={scrollOfTheTop} className={classes.buttonWrapper}>
-			<Image src={arrowIcon} alt='Scroll arrow' />
-		</button>
+		<>
+			{isVisible && (
+				<button onClick={scrollOfTheTop} className={classes.buttonWrapper}>
+					<Image src={arrowIcon} alt='Scroll arrow' />
+				</button>
+			)}
+		</>
 	);
 }
