17109c9d80b3b668b8f8153397fbb7caee6cd50b
Daniel <pandaniel13286@gmail.com>
2024-09-06 20:13:47 +0200

    Add animation to image list
    Add show animation to image list components

--- src/components/imagesList/imagesList.tsx
+++ src/components/imagesList/imagesList.tsx
@@ -7,21 +7,27 @@ import { ImagesType } from '@/types/imagesType';
 export default function ImagesList({ data: images }: { data: ImagesType[] }) {
 	return (
 		<div className={classes.imagesWrapper}>
-			{images.map(image => {
+			{images.map((image, index, array) => {
+				let animationDelay = index / array.length;
+
+				if (animationDelay >= 0.5) {
+					animationDelay = animationDelay / 2;
+				}
 				return (
 					<Link
-						className={`${classes.imageWrapper} ${
+						className={`${classes.imageWrapper} ${classes.animation} ${
 							image.purity === 'sfw' ? classes.outlineSFW : undefined
 						} ${image.purity === 'sketchy' ? classes.outlineSKETCHY : undefined} ${
 							image.purity === 'nsfw' ? classes.outlineNSFW : undefined
 						}`}
 						href={`/${image.id}`}
-						key={image.id}>
+						key={image.id}
+						style={{ animationDelay: `${animationDelay}s` }}>
 						<Image
 							src={image.thumbs.small.replace('//', '/')}
 							alt={'Image'}
 							fill={true}
-							objectFit={'cover'}
+							sizes={'100%'}
 							quality={100}
 						/>
 					</Link>
