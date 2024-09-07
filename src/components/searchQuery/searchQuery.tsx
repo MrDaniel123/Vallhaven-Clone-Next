51b40a5e8e93e58a02f81561393105c5b401db88
Daniel <pandaniel13286@gmail.com>
2024-09-07 12:11:38 +0200

    Fix SearchQuery query bug
    Fixed bug that caused the query not to be
    displayed in the search

--- src/components/searchQuery/searchQuery.tsx
+++ src/components/searchQuery/searchQuery.tsx
@@ -1,17 +1,31 @@
 'use client';
 
-import { ChangeEvent, useContext, useState } from 'react';
+import { ChangeEvent, useContext, useEffect, useState } from 'react';
 import Image from 'next/image';
 
 import classes from './searchQuery.module.scss';
 import loupe from '@/assets/Loupe.png';
 import Link from 'next/link';
 import { SearchContext } from '@/context/context';
-import paramsGenerator from '@/helpes/paramsGenerator';
+import { useParams, useSearchParams } from 'next/navigation';
+
+interface ImagesParams {
+	categories: string;
+	purity: string;
+	sorting: string;
+	query: string;
+}
 
 export default function SearchQuery({ isMobile }: { isMobile?: boolean }) {
 	const { dispatch } = useContext(SearchContext);
-	const [query, setQuery] = useState('');
+	const [query, setQuery] = useState<string | null>('');
+
+	const searchParams = useSearchParams();
+	const queryParam = searchParams.get('query');
+
+	useEffect(() => {
+		setQuery(queryParam);
+	}, [queryParam]);
 
 	function handleInputChange(e: ChangeEvent<HTMLInputElement>) {
 		setQuery(e.target.value);
@@ -23,7 +37,13 @@ export default function SearchQuery({ isMobile }: { isMobile?: boolean }) {
 
 	return (
 		<div className={`${classes.searchQuery} ${isMobile ? classes.mobile : classes.desktop}`}>
-			<input type='text' name='' id='' value={query} onChange={e => handleInputChange(e)} />
+			<input
+				type='text'
+				name=''
+				id=''
+				value={query ? query : ''}
+				onChange={e => handleInputChange(e)}
+			/>
 			<Link
 				href={{
 					pathname: '/images',
@@ -34,7 +54,7 @@ export default function SearchQuery({ isMobile }: { isMobile?: boolean }) {
 						query: query,
 					},
 				}}>
-				<button onClick={() => onCLickHandle(query)}>
+				<button onClick={() => onCLickHandle(query ? query : '')}>
 					<Image src={loupe} alt={'Loupe'}></Image>
 				</button>
 			</Link>
