d4cf9cfbed3186ad2922352a0834e9df4e61ba01
Daniel <pandaniel13286@gmail.com>
2024-09-08 12:36:52 +0200

    Add hide or show mobile menu on scroll

--- src/features/menu/menu.tsx
+++ src/features/menu/menu.tsx
@@ -1,15 +1,33 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import SearchQuery from '@/components/searchQuery/searchQuery';
 import MenuMobile from './menuMobile';
 import MenuDesktop from './menuDesktop';
 
 import classes from './menu.module.scss';
-import OptionBtn from './optionBtn';
 
 export default function Menu() {
 	const [menuIsOpen, setMenuIsOpen] = useState(false);
+	const [showOnScroll, setShowOnScroll] = useState(true);
+	const [lastScrollY, setLastScrollY] = useState(0);
+
+	const controlNavbar = () => {
+		if (window.scrollY > lastScrollY) {
+			setShowOnScroll(false);
+		} else {
+			setShowOnScroll(true);
+		}
+
+		setLastScrollY(window.scrollY);
+	};
+
+	useEffect(() => {
+		window.addEventListener('scroll', controlNavbar);
+		return () => {
+			window.removeEventListener('scroll', controlNavbar);
+		};
+	}, [lastScrollY]);
 
 	const handleOpenMenu = () => {
 		setMenuIsOpen(!menuIsOpen);
@@ -17,7 +35,10 @@ export default function Menu() {
 
 	return (
 		<>
-			<div className={classes.menuWrapper}>
+			<div
+				className={`${classes.menuWrapper}  ${
+					showOnScroll ? classes.showOnScroll : classes.hiddenOnScroll
+				}`}>
 				<div className={classes.searchQueryWrapper}>
 					<SearchQuery isMobile={true} />
 				</div>
